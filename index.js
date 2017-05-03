56f1c35d2d40f0ce67329d15419927a750b4b49f
Julien Fontanet <julien.fontanet@isonoe.net>
2017-05-03 16:25:47 +0200

    chore: explicit comparisons to `undefined`

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function bindPropertyDescriptor (descriptor, thisArg) {
   descriptor = assign({}, descriptor)
 
   var get = descriptor.get
-  if (get) {
+  if (get !== undefined) {
     descriptor.get = bind(get, thisArg)
   }
 
   var set = descriptor.set
-  if (set) {
+  if (set !== undefined) {
     descriptor.set = bind(set, thisArg)
   }
 
@@ -29,7 +29,8 @@ exports.default = bindPropertyDescriptor
 
 function getBoundPropertyDescriptor (obj, prop, thisArg) {
   var descriptor = Object.getOwnPropertyDescriptor(obj, prop)
-
-  return descriptor && bindPropertyDescriptor(descriptor, thisArg || obj)
+  if (descriptor !== undefined) {
+    return bindPropertyDescriptor(descriptor, thisArg !== undefined ? thisArg : obj)
+  }
 }
 exports.getBoundPropertyDescriptor = getBoundPropertyDescriptor
